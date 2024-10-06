fb6b6fbb0d90f4adffb066ae91a9d209f35fdd6f
Silas Hanyecz <silas.hanyecz@mysign.ch>
2024-10-06 20:11:57 +0200

    Mapping

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from "../views/home.vue";
+import Project from "../views/project.vue";
+import Tutorial from "../views/tutorial.vue";
+import Info from "../views/info.vue";
+import Contact from "../views/contact.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +12,28 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: Home
-        }]
+        },
+        {
+            path: '/project',
+            name: 'project',
+            component: Project
+        },
+        {
+            path: '/tutorial',
+            name: 'tutorial',
+            component: Tutorial
+        },
+        {
+            path: '/info',
+            name: 'info',
+            component: Info
+
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: Contact
+        }
+    ]
 })
 export default router
