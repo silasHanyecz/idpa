0de32303d3dcf54535e8775e80efc81dd6449376
Silas Hanyecz <silas.hanyecz@mysign.ch>
2024-11-10 17:14:39 +0100

    include main.css

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
 import {fas} from "@fortawesome/free-solid-svg-icons"
 import {far} from "@fortawesome/free-regular-svg-icons"
+import './assets/main.css';
 
 library.add(fas, far)
 const app = createApp(App)
