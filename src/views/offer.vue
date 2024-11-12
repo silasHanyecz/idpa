8dcf72fe1302a9813174e0387ce03854a86cf449
Silas Hanyecz <silas.hanyecz@mysign.ch>
2024-11-12 18:49:08 +0100

    offerbox

--- src/views/offer.vue
+++ src/views/offer.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="container">
-    <h1>VideoAgenturName</h1>
+
+    <div class="row d-flex justify-content-center mt-5">
+      <div class="offerbox col-1">
+
+      </div>
+      <div class="offerbox col-1 ms-5">
+
+      </div>
+      <div class="offerbox col-1 ms-5">
+
+      </div>
+    </div>
+
   </div>
 </template>
+
 <script setup>
 </script>
