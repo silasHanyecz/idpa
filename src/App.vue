15f0dbf30fe1a3fb3f885169190881ed12d7dda4
brave187 <166106241+brave187@users.noreply.github.com>
2024-11-19 19:28:19 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {RouterLink, RouterView} from 'vue-router'
-import Navigation from "@/components/Navigation.vue";
+import Navigation from "@/components/navigation.vue";
 import Footer from "@/components/footer.vue";
 
 </script>
