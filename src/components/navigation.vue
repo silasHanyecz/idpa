1414f5cb7c8e2cba796064db01f6194a005aca57
Silas Hanyecz <silas.hanyecz@mysign.ch>
2024-11-20 11:05:53 +0100

    sticky navigation

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,38 +1,42 @@
 <script setup>
 </script>
 
-<template >
-<h1 class="text-center p-3 mb-0">EHB - Film works</h1>
-  <nav class="navbar navbar-expand-lg sticky-top" >
+<template>
+  <nav class="navbar navbar-expand-lg sticky-top navigation">
     <div class="container">
+      <li class="nav-item removeDot">
+        <router-link to="/">
+          <img src="/img/Logo_EHB_Filmworks_SW.png" width="150px" class="mt-5 me-5 mb-5">
+        </router-link>
+      </li>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#Toggler"
               aria-controls="Toggler" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse justify-content-center text h2 " id="Toggler">
+      <div class="w-75">
+      <div class="collapse navbar-collapse justify-content-start borderXwidth " id="Toggler">
 
-        <ul class="navbar-nav align-items-end">
-          <li class="nav-item">
-            <router-link class="nav-link" style="color: white" to="/">Home</router-link>
-          </li>
+        <ul class="navbar-nav">
 
           <li class="nav-item">
-            <router-link class="nav-link" style="color: white" to="/offer">Angebot</router-link>
+            <router-link class="nav-link" to="/offer">Angebot</router-link>
           </li>
 
           <li class="nav-item">
-            <router-link class="nav-link" style="color: white" to="/projects">Projekte</router-link>
+            <router-link class="nav-link" to="/projects">Projekte</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" style="color: white" to="/tutorial">Drohne</router-link>
+            <router-link class="nav-link" to="/tutorial">Drohne</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" style="color: white" to="/info">Über uns</router-link>
+            <router-link class="nav-link" to="/info">Über uns</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" style="color: white" to="/contact">Kontakt</router-link>
+            <router-link class="nav-link" to="/contact">Kontakt</router-link>
           </li>
         </ul>
+
+      </div>
       </div>
     </div>
   </nav>
