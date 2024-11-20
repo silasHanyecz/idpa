748e912be220706a5bfadd4d191b480a2305edae
Silas Hanyecz <silas.hanyecz@mysign.ch>
2024-11-20 11:06:09 +0100

    offer demo black box

--- src/views/offer.vue
+++ src/views/offer.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="container">
-
+    <h1 class="text-center mt-5">Angebot</h1>
     <div class="row d-flex justify-content-center mt-5">
       <div class="offerbox col-1">
-
+        <h2 class="text-center pt-3">One-Take</h2>
       </div>
       <div class="offerbox col-1 ms-5">
-
+        <h2 class="text-center pt-3">Reels</h2>
       </div>
       <div class="offerbox col-1 ms-5">
-
+        <h2 class="text-center pt-3">Event Edit</h2>
+      </div>
+    </div>
+    <div class="row d-flex justify-content-center align-items-center mt-5">
+      <div class="col-auto d-flex align-items-center me-3">
+        <div class="rectangle">
+          <h2 class="text-center">Planung</h2>
+        </div>
+        <div class="triangle-right"></div>
+      </div>
+      <div class="col-auto d-flex align-items-center me-3">
+        <div class="rectangle">
+          <h2 class="text-center">Planung</h2>
+        </div>
+        <div class="triangle-right"></div>
+      </div>
+      <div class="col-auto d-flex align-items-center me-3">
+        <div class="rectangle">
+          <h2 class="text-center">Planung</h2>
+        </div>
+        <div class="triangle-right"></div>
       </div>
     </div>
 
