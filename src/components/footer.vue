cf3a5f910ed797cd4a7b1d03f1e5eb0de2120e96
Silas Hanyecz <silas.hanyecz@mysign.ch>
2024-12-06 10:48:26 +0100

    footer v2

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -9,18 +9,21 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
         <div class="col-xl-4 col-md-4 mb-30">
           <div class="single-cta">
             <i class="fas fa-map-marker-alt"></i>
-            <div class="cta-text">
-              <h4>Find us</h4>
-              <span>1010 Avenue, sw 54321, chandigarh</span>
+            <div class="footer-widget">
+              <div class="footer-logo">
+                <img src="/img/Logo_EHB_Filmworks_SW.png" class="img-fluid" alt="logo">
+              </div>
             </div>
           </div>
         </div>
         <div class="col-xl-4 col-md-4 mb-30">
           <div class="single-cta">
-            <i class="fas fa-phone"></i>
             <div class="cta-text">
-              <h4>Call us</h4>
-              <span>9876543210 0</span>
+              <h4>Follow Us</h4>
+              <div class="footer-social-icon">
+                <a href="#" class="m-0 p-0"><font-awesome-icon :icon="['far', 'envelope']" style="color: white" /></a>
+                <a href="#" class="m-0 p-0 ps-3"><font-awesome-icon :icon="['fab', 'instagram']" style="color: white"/></a>
+              </div>
             </div>
           </div>
         </div>
@@ -29,30 +32,7 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
             <i class="far fa-envelope-open"></i>
             <div class="cta-text">
               <h4>Mail us</h4>
-              <span>mail@info.com</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="footer-content pt-5 pb-5">
-      <div class="row">
-        <div class="col-xl-4 col-lg-4 mb-50">
-          <div class="footer-widget">
-            <div class="footer-logo">
-              <a href="index.html"><img src="https://i.ibb.co/QDy827D/ak-logo.png" class="img-fluid" alt="logo"></a>
-            </div>
-          </div>
-        </div>
-        <div class="col-xl-4 col-md-4 mb-30">
-          <div class="single-cta">
-            <i class="fas fa-map-marker-alt"></i>
-            <div class="cta-text">
-              <h4>Follow Us</h4>
-              <div class="footer-social-icon">
-                <a href="#"><font-awesome-icon :icon="['far', 'envelope']" style="color: white" /></a>
-                <a href="#"><font-awesome-icon :icon="['fab', 'instagram']" style="color: white"/></a>
-              </div>
+              <a  class="m-0 p-0 pt-5" href="mailto:ehb@gmail.com" style="color: white; font-size: 15px">ehb@gmail.com</a>
             </div>
           </div>
         </div>
@@ -65,9 +45,9 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
         <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
           <div class="footer-menu">
             <ul>
-              <li><a>
+              <li>
                 <router-link to="/">Home</router-link>
-                Home</a></li>
+              </li>
               <li><a href="#">Terms</a></li>
               <li><a href="#">Privacy</a></li>
               <li><a href="#">Policy</a></li>
